8561ce961affece4826641c09ddbee12d844a8f3
Ubuntu <ubuntu@ip-172-31-10-95.us-west-2.compute.internal>
2013-07-07 20:44:02 +0000

    test

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- 
+  var fs = require('fs'); 
   response.send(content);
 });
 
