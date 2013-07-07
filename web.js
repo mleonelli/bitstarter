f145005acfd0784d93bf0fb0c7413b2a0da39bf0
Ubuntu <ubuntu@ip-172-31-10-95.us-west-2.compute.internal>
2013-07-07 20:42:19 +0000

    ora?

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var fs = require('fs');
-  var content = fs.readFile('index.html');
+ 
   response.send(content);
 });
 
